8b36f5214cabfd6c54e4b5b1b6ce19403917baaa
Lily Lu <lilyzhaoyilu@gmail.com>
2022-12-25 16:57:11 -0800

    Add querying live status and some dynamic function

--- src/commands/ms/streamers.ts
+++ src/commands/ms/streamers.ts
@@ -1,4 +1,5 @@
-import { AppCommand, AppFunc, BaseSession } from 'kbotify';
+import { Card, AppCommand, AppFunc, BaseSession } from 'kbotify';
+const axios = require('axios');
 
 class Streamers extends AppCommand {
   code = 'streamers'; // 只是用作标记
@@ -6,108 +7,126 @@ class Streamers extends AppCommand {
   help = '`查询所有MS主播的直播间链接`'; // 帮助文字
   intro = '复读你所说的文字, 并用kmarkdown格式返回。';
   func: AppFunc<BaseSession> = async (session) => {
-    return session.sendCard(constructCard());
+    const card = await constructCard(streamersList);
+    return session.sendCard(card);
   };
 }
 
 export const streamers = new Streamers();
 
+// {roomNumber : isDouyu}
+const streamersList = {
+  '5684726': true, // 皮特174
+  '1667826': true, // 一口三明治3Mz
+  '10722927': true, // Roieee百弟DF
+  '6490082': true, // Jackyar
+  '11123690': true, // 波靈BoLin
+  '26161543': false, // QQ
+}
 
-const constructCard = () => {
-  return `[
+interface LiveData {
+  streamerName: string,
+  roomName: string,
+  url: string,
+  isStreaming: boolean,
+  avatar: string,
+  roomThumb: string,
+}
+
+const getDouyuLiveData = async (roomNumber: string) => {
+  const response = await axios.get(`http://open.douyucdn.cn/api/RoomApi/room/${roomNumber}`)
+  const data = response.data.data;
+  const res: LiveData = { streamerName: data.owner_name, roomName: data.room_name, url: `https://www.douyu.com/${roomNumber}`, isStreaming: data.online !== 0, avatar: data.avatar, roomThumb: data.room_thumb }
+  return res
+}
+
+// hard coding
+const getBiliBiliLiveData = async (roomNumber: string) => {
+  const response = await axios.get(`http://api.live.bilibili.com/room/v1/Room/get_info?room_id=${roomNumber}`)
+  const data = response.data.data;
+  // Bilibili has 2 for broadcasting recording
+  const isStreaming = data.live_status !== 0 ? true : false;
+  const res: LiveData = { streamerName: 'QQ', roomName: data.title, url: `https://live.bilibili.com/${roomNumber}`, isStreaming: isStreaming, avatar: 'https://i1.hdslb.com/bfs/face/ac2c6d3112c9e97db9459ed89019c09b9b1bf431.jpg', roomThumb: data.user_cover }
+  return res
+}
+
+const buildStreamersCardSection = async (streamerRoomNumber: string, isDouyuStreamer = true) => {
+  const res = isDouyuStreamer ? await getDouyuLiveData(streamerRoomNumber) : await getBiliBiliLiveData(streamerRoomNumber);
+  const streamingText = res.isStreaming ? "*直播中*" : "";
+
+  const regularCard = `{
+    "type": "section",
+    "text": {
+      "type": "kmarkdown",
+      "content": "[${res.streamerName}](${res.url})  ${streamingText}"
+    },
+    "mode": "left",
+    "accessory": {
+      "type": "image",
+      "src": "${res.avatar}",
+      "size": "lg"
+    }
+  }`
+
+  let liveCard = '';
+  if (res.isStreaming) {
+    // 有承接的逗号，结尾没有逗号
+    liveCard = `,{
+      "type": "section",
+      "text": {
+        "type": "kmarkdown",
+        "content": "${res.roomName}"
+      }
+    },
+    {
+      "type": "container",
+      "elements": [
         {
-          "type": "card",
-          "theme": "secondary",
-          "size": "lg",
-          "modules": [
-            {
-              "type": "header",
-              "text": {
-                "type": "plain-text",
-                "content": "MS 主播直播间一览"
-              }
-            },
-            {
-              "type": "divider"
-            },
-            {
-              "type": "section",
-              "text": {
-                "type": "kmarkdown",
-                "content": "[皮特174的直播间](https://www.douyu.com/5684726)"
-              },
-              "mode": "left",
-              "accessory": {
-                "type": "image",
-                "src": "https://apic.douyucdn.cn/upload/avatar_v3/202107/573dc76d4d7e4994b6b55e1df28d72b0_big.jpg",
-                "size": "lg"
-              }
-            },
-            {
-              "type": "section",
-              "text": {
-                "type": "kmarkdown",
-                "content": "[一口三明治3Mz的直播间](https://www.douyu.com/1667826)"
-              },
-              "mode": "left",
-              "accessory": {
-                "type": "image",
-                "src": "https://apic.douyucdn.cn/upload/avatar_v3/202209/d5b9d408f15f44129094fe2c8d51e7ee_big.jpg",
-                "size": "lg"
-              }
-            },
-            {
-              "type": "section",
-              "text": {
-                "type": "kmarkdown",
-                "content": "[Roieee百弟DF的直播间](https://www.douyu.com/10722927)"
-              },
-              "mode": "left",
-              "accessory": {
-                "type": "image",
-                "src": "https://apic.douyucdn.cn/upload/avatar_v3/202203/f08daf1b788643b791954ee6b5af50c7_big.jpg",
-                "size": "lg"
-              }
-            },
-            {
-              "type": "section",
-              "text": {
-                "type": "kmarkdown",
-                "content": "[Jackyar的直播间](https://www.douyu.com/6490082)"
-              },
-              "mode": "left",
-              "accessory": {
-                "type": "image",
-                "src": "https://apic.douyucdn.cn/upload/avatar_v3/202107/e3170c90309547b5bb0bc38b8cae0a16_big.jpg",
-                "size": "lg"
-              }
-            },
-            {
-              "type": "section",
-              "text": {
-                "type": "kmarkdown",
-                "content": "[波靈BoLin的直播间](https://www.douyu.com/11123690)"
-              },
-              "mode": "left",
-              "accessory": {
-                "type": "image",
-                "src": "https://apic.douyucdn.cn/upload/avatar_v3/202209/2886039f798749f8b08ac32306eb67ee_big.jpg",
-                "size": "lg"
-              }
-            },{
-              "type": "section",
-              "text": {
-                "type": "kmarkdown",
-                "content": "[QQ的直播间](https://live.bilibili.com/26161543)"
-              },
-              "mode": "left",
-              "accessory": {
-                "type": "image",
-                "src": "https://i1.hdslb.com/bfs/face/ac2c6d3112c9e97db9459ed89019c09b9b1bf431.jpg",
-                "size": "lg"
-              }
-            }
-          ]
+          "type": "image",
+          "src": "${res.roomThumb}"
         }
-      ]`
+      ]
+    }`
+  }
+
+  return { cardText: regularCard + liveCard, isStreaming: res.isStreaming };
+
+}
+
+
+const constructCard = async (streamersList: Object) => {
+
+  let card = `[
+    {
+      "type": "card",
+      "theme": "secondary",
+      "size": "lg",
+      "modules": [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain-text",
+            "content": " MS 主播一览"
+          }
+        },
+        {
+          "type": "divider"
+        },`
+
+  let laterPartCard = '';
+  const cardTail = `]}]`
+  for (const [roomNumber, isDouyu] of Object.entries(streamersList)) {
+    const res = await buildStreamersCardSection(roomNumber, isDouyu);
+    if (res.isStreaming) {
+      card += res.cardText;
+      card += ','
+    } else {
+      laterPartCard += res.cardText;
+      laterPartCard += ','
+    }
+  }
+
+  laterPartCard = laterPartCard.slice(0, -1);
+
+  return card + laterPartCard + cardTail;
 }
